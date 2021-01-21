e225babb7078864d4aaac5880877a62554070280
dbeod2 <darung.0211@gmail.com>
2021-01-21 21:20:34 +0900

    test : spring bean 조회 테스트 코드 추가

---
+++ src/test/java/springmain/beanfind/ApplicationContextBasicFindTest.java
@@ -0,0 +1,40 @@
+package springmain.beanfind;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import springmain.core.AppConfig;
+import springmain.core.member.service.MemberService;
+import springmain.core.member.service.MemberServiceImpl;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class ApplicationContextBasicFindTest {
+
+  AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+  @Test
+  @DisplayName("빈 이름으로 조회")
+  public void findBeanByName() {
+    MemberService memberService = ac.getBean("memberService", MemberService.class);
+    assertThat(memberService).isInstanceOf(MemberService.class);
+  }
+
+  @Test
+  @DisplayName("이름 없이 타입으로 조회")
+  public void findBeanByType() {
+    MemberService memberService = ac.getBean(MemberService.class);
+    assertThat(memberService).isInstanceOf(MemberService.class);
+  }
+
+  @Test
+  @DisplayName("빈 이름 조회 X")
+  public void findBeanByNameX() {
+    assertThrows(NoSuchBeanDefinitionException.class,
+            () -> ac.getBean("test", MemberService.class));
+  }
+}
