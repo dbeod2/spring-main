1145ffde3d7e416ffc690e29653e8e456f19fe73
dbeod2 <darung.0211@gmail.com>
2021-01-24 20:27:35 +0900

    README.md 추가

--- src/test/java/springmain/singleton/ConfigurationSingletonTest.java
+++ src/test/java/springmain/singleton/ConfigurationSingletonTest.java
@@ -25,4 +25,6 @@ public class ConfigurationSingletonTest {
     assertThat(memberService.getMemberRepository()).isSameAs(memberRepository);
     assertThat(orderService.getMemberRepository()).isSameAs(memberRepository);
   }
+
+
 }
